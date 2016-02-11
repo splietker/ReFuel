8b8ecc95be4f8733f98d1831bd7eb5a27575baed
Malte Splietker <maltespl@mail.upb.de>
2016-02-11 02:46:48 +0100

    Separated library from application to enable testing

--- ReFuel/src/main.cpp
+++ ReFuel/src/main.cpp
@@ -5,8 +5,6 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    //Q_INIT_RESOURCE(refuel);
-
     QApplication app(argc, argv);
 
     if (!QSystemTrayIcon::isSystemTrayAvailable()) {
