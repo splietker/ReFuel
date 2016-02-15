2163f75c537ec4e24c1e00a6772d69d94080a5d3
Malte Splietker <maltespl@mail.upb.de>
2016-02-15 23:00:06 +0100

    Added Apache License 2.0 to project
    Signed-off-by: Malte Splietker <maltespl@mail.upb.de>

--- ReFuel/src/BrightnessNotifier.cpp
+++ ReFuel/src/BrightnessNotifier.cpp
@@ -1,6 +1,18 @@
-//
-// Created by msp on 2/11/16.
-//
+/*
+ * Copyright 2016 Malte Splietker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *         limitations under the License.
+ */
 
 #include <cmath>
 
@@ -89,7 +101,7 @@ void BrightnessNotifier::setBrightness(double brightness) {
         XRRCrtcGamma *gamma = XRRAllocGamma(size);
 
         for (int i = 0; i < size; i++) {
-            unsigned short gammaValue = (unsigned short)((double) i / (double) (size - 1) * brightness * 65535.0);
+            unsigned short gammaValue = (unsigned short) ((double) i / (double) (size - 1) * brightness * 65535.0);
             gamma->red[i] = gammaValue;
             gamma->green[i] = gammaValue;
             gamma->blue[i] = gammaValue;
